78699d635051b95b076a680daeeb33d92ef02546
Harit Joshi <harit@aegiscovenant.com>
2023-06-02 08:51:53 +0000

    init setup

---
+++ backend/handler/app.ts
@@ -0,0 +1,21 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+
+export const imageUploadHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'image uploaded successfully',
+            }),
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
